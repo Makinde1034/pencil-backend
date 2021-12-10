b8aa535cf02ea134c2f71f578a2ae06bb01ff278
Makinde1034 <makinde1034@gmail.com>
2021-12-10 16:03:42 +0100

    added cors to signin route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -5,8 +5,8 @@ const cors = require("cors")
 const authRoute = express.Router();
 
 authRoute.post("/signup",authController.signUp);
-authRoute.get("/verify/:accessToken",authController.verifyAccount);
 authRoute.post("/signin",cors(),authController.signIn);
+authRoute.get("/verify/:accessToken",authController.verifyAccount);
 authRoute.get("/user",authController.findUser);
 
 
