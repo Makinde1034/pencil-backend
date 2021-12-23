eb7b613c22cec11bee9500aee9d604bbd5da9fb4
Makinde1034 <makinde1034@gmail.com>
2021-12-23 20:11:55 +0100

    updates

--- main.js
+++ main.js
@@ -9,18 +9,18 @@ const durl = "mongodb+srv://Makinde1034:Makinde1034@pencil.2ym6k.mongodb.net/Pen
 
 const app = express();
 
-// const corsOptions = {
-//     origin : "*",
-//     optionsSuccessStatus : 200
-// };
+const corsOptions = {
+    origin : "*",
+    optionsSuccessStatus : 200
+};
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
-app.use(function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//    res.header("Access-Control-Allow-Origin", "*");
+//    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 
 
